e97f1ffb7a13cad888c76cb5012503c8fe80655b
Noah Gift <noah.gift@gmail.com>
2020-08-09 01:20:31 +0000

    adding cli

--- hello.py
+++ hello.py
@@ -1,2 +1,20 @@
-#var=
-print("hello")
+#!/usr/bin/env python
+import click
+
+def api(key,phrase):
+    buslogic = "%s is great %s" % (key, phrase) #pretend api call
+    return buslogic
+
+#print(api("1234", "John"))
+
+@click.command()
+@click.option('--key')
+@click.option('--phrase')
+def callapi(key, phrase):
+    results = api(key,phrase)
+    print(results)
+    
+if __name__ == '__main__':
+    #pylint: disable=no-value-for-parameter
+    callapi()
+
